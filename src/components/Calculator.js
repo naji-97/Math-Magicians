8327d15cd9874e530e3624f6f64aa0b7df8acb87
Naji-97 <elbairabennaji@gmail.com>
2023-02-21 16:31:34 +0100

    Calculator component and style it

---
+++ src/components/Calculator.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './Calculator.css';
+
+function Calculator() {
+  return (
+    <div className="calculator">
+      <div className="display">0</div>
+      <div className="button-row">
+        <button type="button" className="button-clear">AC</button>
+        <button type="button" className="button-sign">+/-</button>
+        <button type="button" className="button-percent">%</button>
+        <button type="button" className="button-operator">÷</button>
+      </div>
+      <div className="button-row">
+        <button type="button" className="button-number">7</button>
+        <button type="button" className="button-number">8</button>
+        <button type="button" className="button-number">9</button>
+        <button type="button" className="button-operator">×</button>
+      </div>
+      <div className="button-row">
+        <button type="button" className="button-number">4</button>
+        <button type="button" className="button-number">5</button>
+        <button type="button" className="button-number">6</button>
+        <button type="button" className="button-operator">-</button>
+      </div>
+      <div className="button-row">
+        <button type="button" className="button-number">1</button>
+        <button type="button" className="button-number">2</button>
+        <button type="button" className="button-number">3</button>
+        <button type="button" className="button-operator">+</button>
+      </div>
+      <div className="button-row">
+        <button type="button" className="button-number button-zero">0</button>
+        <button type="button" className="button-number">.</button>
+        <button type="button" className="button-operator">=</button>
+      </div>
+    </div>
+  );
+}
+
+export default Calculator;
