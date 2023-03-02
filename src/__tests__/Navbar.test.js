25f46c0b02d3766c445575f4ef904a3e59840b32
rica213 <25f5k252l@mozmail.com>
2023-03-02 18:51:31 +0300

    Add test to operate and quote

--- src/__tests__/Navbar.test.js
+++ src/__tests__/Navbar.test.js
@@ -4,23 +4,24 @@ import { BrowserRouter } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 
 describe('Navbar', () => {
- test('renders Navbar component', () => {
-  render(<BrowserRouter> 
-  <Navbar />
-</BrowserRouter>);
-  const linkElements = screen.getAllByTestId(/navbar-link-.*/);
-  expect(linkElements).toHaveLength(4);
-});
-
+  test('renders Navbar component', () => {
+    render(
+      <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>,
+    );
+    const linkElements = screen.getAllByTestId(/navbar-link-.*/);
+    expect(linkElements).toHaveLength(4);
+  });
 
   test('contains the correct links', () => {
     render(
       <BrowserRouter>
         <Navbar />
-      </BrowserRouter>
+      </BrowserRouter>,
     );
     const homeLink = screen.getByRole('link', { name: 'Home' });
-    
+
     const calculatorLink = screen.getByRole('link', { name: 'Calculator' });
     const quoteLink = screen.getByRole('link', { name: 'Quote' });
     expect(homeLink.getAttribute('href')).toBe('/');
